c596e8b8f6b41c4fed9013b046cf1c906455a187
jsk890 <resqme89@naver.com>
2022-05-25 13:47:22 +0900

    아스키코드

---
+++ Bronze/11654.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    /*아스키코드 11654*/
+    char ch;
+    cin >> ch;
+    int chtoi = ch;
+    cout << chtoi;
+}
