1dc50eb885dcbae6f8f101d1ad7a55433be58fcb
jsk890 <resqme89@naver.com>
2022-05-25 14:43:03 +0900

    크로아티아 알파벳

---
+++ Silver/2941.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/*크로아티아 알파벳 2941*/
+	string str;
+	int cnt = 0;
+	getline(cin, str);
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str.substr(i, 2) == "c=") { i++; }
+		else if (str.substr(i, 2) == "c-") { i++; }
+		else if (str.substr(i, 3) == "dz=") { i += 2; }
+		else if (str.substr(i, 2) == "d-") { i++; }
+		else if (str.substr(i, 2) == "lj") { i++; }
+		else if (str.substr(i, 2) == "nj") { i++; }
+		else if (str.substr(i, 2) == "s=") { i++; }
+		else if (str.substr(i, 2) == "z=") { i++; }
+		cnt++;
+	}
+	cout << cnt;
+}
