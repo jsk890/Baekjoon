ab4da6c8ed3179486bfc61304bb91584872dabcd
jsk890 <resqme89@naver.com>
2022-05-25 15:40:39 +0900

    새싹

---
+++ Bronze/25083.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*»õ½Ï 25083*/
+    cout << "         ,r'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+
+}
