b96be07b66832900f0a77e968a5afb679028f27e
jsk890 <resqme89@naver.com>
2022-05-25 14:27:30 +0900

    사칙연산

---
+++ Bronze/10869.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*사칙연산 10869*/
+    int num1;
+    int num2;
+
+    cin >> num1 >> num2;
+    cout << num1 + num2 << endl;
+    cout << num1 - num2 << endl;
+    cout << num1 * num2 << endl;
+    cout << num1 / num2 << endl;
+    cout << (num1 % num2) << endl;
+
+    return 0;
+}
