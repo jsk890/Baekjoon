d7fad9767371da8a8eaa46e34aee95b1e214e073
jsk890 <resqme89@naver.com>
2022-05-25 14:24:02 +0900

    AxB

---
+++ Bronze/10998.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*AxB 10998*/
+    int num1;
+    int num2;
+
+    cin >> num1 >> num2;
+    cout << num1 * num2;
+
+    return 0;
+}
