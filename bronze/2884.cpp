b6066712999c0d44049f8cad02c5671741322bca
jsk890 <resqme89@naver.com>
2022-05-25 13:04:17 +0900

    알람시계

---
+++ bronze/2884.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    /*알람시계 2884*/
+    int hour=0, minute=0, conHourintoMin=0;
+    cin >> hour >> minute;
+
+    conHourintoMin = hour * 60 + minute;
+    if (conHourintoMin >= 45) conHourintoMin -= 45;
+    else conHourintoMin = 24 * 60 - 45 + minute;
+    minute = conHourintoMin % 60;
+    hour = (int)(conHourintoMin / 60);
+
+    cout << hour << " " << minute << endl;
+}
