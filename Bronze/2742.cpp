d61d65c7c55999420fdcac690a9f71786c539ff7
jsk890 <resqme89@naver.com>
2022-05-25 13:28:48 +0900

    기찍 N

---
+++ Bronze/2742.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    /*±âÂïN 2742*/
+    int num = 0;
+    cin >> num;
+    for (int i = num; i >= 1; i--)
+    {
+        cout << i << "\n";
+    }
+}
