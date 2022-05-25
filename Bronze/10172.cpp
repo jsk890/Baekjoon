01b99c25112d81609587aecbd24b3d9cd4bfe150
jsk890 <resqme89@naver.com>
2022-05-25 14:33:14 +0900

    개

---
+++ Bronze/10172.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*°³ 10172*/
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+
+    return 0;
+}
