42c93d232438bd98d19ab19ebd70d8a9d6caed02
jsk890 <resqme89@naver.com>
2022-05-25 15:25:22 +0900

    Hello World

---
+++ Bronze/2557.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    /*Hello World 2557*/
+    cout << "Hello World!\n";
+
+    return 0;
+}
