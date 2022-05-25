9c568c34f029b46298272116fbbb855acf50346a
jsk890 <resqme89@naver.com>
2022-05-25 13:29:45 +0900

    A+B-7

---
+++ Bronze/11021.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    /*A+B-7 11021*/
+    int a = 0, b = 0, cnt = 0;
+    cin >> cnt;
+    for (int i = 1; i <= cnt; i++)
+    {
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a + b << "\n";
+    }
+}
