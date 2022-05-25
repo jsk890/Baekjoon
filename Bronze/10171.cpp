5e6189adf2441d2176cf1ccaa7c74bdf58493c5d
jsk890 <resqme89@naver.com>
2022-05-25 14:34:56 +0900

    고양이

---
+++ Bronze/10171.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*°í¾çÀÌ 10171*/
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+
+    return 0;
+}
