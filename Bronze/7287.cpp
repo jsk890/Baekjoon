14aba8e5da9365220b0dda6294bf12b8f6797d11
jsk890 <resqme89@naver.com>
2022-06-15 13:58:02 +0900

    등록

---
+++ Bronze/7287.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/*µî·Ï 7287*/
+	cout << "125" << "\n";
+	cout << "resqme89" << "\n";
+}
