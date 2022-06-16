2f6bdd66ec017cf630600ab482f74e90b9392dab
jsk890 <resqme89@naver.com>
2022-06-16 13:56:58 +0900

    콜센터

---
+++ Bronze/5339.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/*ÄÝ¼¾ÅÍ 5339*/
+	cout << "     /~\\" << "\n";
+	cout << "    ( oo|" << "\n";
+	cout << "    _\\=/_" << "\n";
+	cout << "   /  _  \\" << "\n";
+	cout << "  //|/.\\|\\\\" << "\n";
+	cout << " ||  \\ /  ||" << "\n";
+	cout << "============" << "\n";
+	cout << "|          |" << "\n";
+	cout << "|          |" << "\n";
+	cout << "|          |" << "\n";
+}
