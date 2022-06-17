265d93d5dcc540f3cc94ba985a3c82de3c7ab486
jsk890 <resqme89@naver.com>
2022-06-17 09:35:21 +0900

    꼬마 정민

---
+++ Bronze/11382.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define ull unsigned long long
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/*²¿¸¶ Á¤¹Î 11382 */
+	ull a, b, c;
+	cin >> a >> b >> c;
+	cout << a + b + c;
+}
