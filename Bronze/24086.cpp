4326790fedaf005c16a2ae1edbf79eb06a027d40
jsk890 <resqme89@naver.com>
2022-06-19 00:59:08 +0900

    身長 (Height)

---
+++ Bronze/24086.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/*гуню (Height) 24086 */
+	int a, b;
+	cin >> a >> b;
+	cout << b - a;
+}
