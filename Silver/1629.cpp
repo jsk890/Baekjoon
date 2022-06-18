ba51e5b43ff7c1ecc4b4ca38959b02df4941cb27
jsk890 <resqme89@naver.com>
2022-06-18 21:39:30 +0900

    곱셈

---
+++ Silver/1629.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#define ull unsigned long long
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/*°ö¼À 1629 */
+	ull a, x, i, powval, div;
+	int mul = 1, digit = 1;
+	vector<ull> vecArr;
+	cin >> a >> x >> div;
+	i = x;
+	powval = a % div;
+	while (i)
+	{
+		if (i & 1) vecArr.push_back(powval % div);
+		powval = powval * powval % div;
+		i >>= 1;
+	}
+	for (int i = 0; i < vecArr.size(); i++)
+	{
+		mul = mul * vecArr[i] % div;
+	}
+	cout << mul;
+}
