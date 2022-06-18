ad9aaea0ca595c4d87a6d45b3c0126ef65876212
jsk890 <resqme89@naver.com>
2022-06-19 00:23:48 +0900

    연세대학교

---
+++ Bronze/15680.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/*연세대학교 15680 */
+	int a;
+	cin >> a;
+
+	if (a)	cout << "Leading the Way to the Future"<<"\n";
+	if (!a)	cout << "YONSEI"<<"\n";
+}
