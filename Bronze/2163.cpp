6d9dd91f6c10e4a79e4c81713db88bc86c15581f
jsk890 <resqme89@naver.com>
2022-06-19 00:45:40 +0900

    초콜릿 자르기

---
+++ Bronze/2163.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/*초콜릿 자르기 2163 */
+	int a, b, t, cnt=0;
+	cin >> a >> b;
+	cout << a * b -1;
+}
