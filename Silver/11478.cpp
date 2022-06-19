e658af0971f55b1e011cbf2737717932f8eac85e
jsk890 <resqme89@naver.com>
2022-06-19 17:44:45 +0900

    서로 다른 부분 문자열의 개수

--- Silver/11478.cpp
+++ Silver/11478.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <set>
 using namespace std;
 
 int main()
@@ -10,14 +10,38 @@ int main()
 	ios_base::sync_with_stdio(false);
 	/*서로 다른 부분 문자열의 개수 11478 */
 	string s;
-	map<string, int> mapIntArr;
+	set<string> setIntArr;
 	cin >> s;
 	for (int i = 1; i <= s.size(); i++)
 	{
 		for (int j = 0; j <= s.size() - i; j++)
 		{
-			mapIntArr.insert({ s.substr(j, i),1 });
+			setIntArr.insert( s.substr(j, i));
 		}
 	}
-	cout << mapIntArr.size()<<"\n";
+	cout << setIntArr.size() << "\n";
 }
+
+//#include <iostream>
+//#include <string>
+//#include <map>
+//using namespace std;
+//
+//int main()
+//{
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//	ios_base::sync_with_stdio(false);
+//	/*서로 다른 부분 문자열의 개수 11478 */
+//	string s;
+//	map<string, int> mapIntArr;
+//	cin >> s;
+//	for (int i = 1; i <= s.size(); i++)
+//	{
+//		for (int j = 0; j <= s.size() - i; j++)
+//		{
+//			mapIntArr.insert({ s.substr(j, i),1 });
+//		}
+//	}
+//	cout << mapIntArr.size()<<"\n";
+//}
