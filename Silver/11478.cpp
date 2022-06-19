e2348d54f197f6c346f89ad0b2bdceb4b819c560
jsk890 <resqme89@naver.com>
2022-06-19 17:32:06 +0900

    서로 다른 부분 문자열의 개수

---
+++ Silver/11478.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/*서로 다른 부분 문자열의 개수 11478 */
+	string s;
+	map<string, int> mapIntArr;
+	cin >> s;
+	for (int i = 1; i <= s.size(); i++)
+	{
+		for (int j = 0; j <= s.size() - i; j++)
+		{
+			mapIntArr.insert({ s.substr(j, i),1 });
+		}
+	}
+	cout << mapIntArr.size()<<"\n";
+}
