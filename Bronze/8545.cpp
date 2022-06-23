452908c4dc1cee40231c17290b2469e8703c58c6
jsk890 <resqme89@naver.com>
2022-06-23 16:54:54 +0900

    Zadanie próbne

---
+++ Bronze/8545.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/* Zadanie próbne 8545 */
+	string s;
+	cin >> s;
+	reverse(s.begin(), s.end());
+	cout << s;
+}
