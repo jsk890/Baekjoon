e145ecfe5abff48287d61c38309013a284183281
jsk890 <resqme89@naver.com>
2022-06-26 18:21:01 +0900

    R2

---
+++ Bronze/3046.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/* R2 3046 */
+	int r1, s;
+	cin >> r1 >> s;
+	cout << s * 2 - r1 << "\n";
+}
