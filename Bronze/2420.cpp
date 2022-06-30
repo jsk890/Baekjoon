3f14fa5f0454da6ad6418f4e8aa64d47976bb2a0
jsk890 <resqme89@naver.com>
2022-06-30 09:25:35 +0900

    사파리월드

---
+++ Bronze/2420.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/* 사파리월드 2420 */
+	long long n, m;
+	cin >> n >> m;
+	cout << abs(n - m);
+}
