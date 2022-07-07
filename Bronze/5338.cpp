6f7aad5db28952457489a2c6ddec416bab13882d
jsk890 <resqme89@naver.com>
2022-07-07 13:15:46 +0900

    마이크로소프트 로고

---
+++ Bronze/5338.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	/*마이크로소프트 로고 5338*/
+	cout << "       _.-;;-._" << "\n";
+	cout << "'-..-'|   ||   |" << "\n";
+	cout << "'-..-'|_.-;;-._|" << "\n";
+	cout << "'-..-'|   ||   |" << "\n";
+	cout << "'-..-'|_.-''-._|" << "\n";
+}
